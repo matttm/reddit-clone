06979b3315a40c1e38566fc556afe47d2d9fc35a
matttm <matttmaloney@gmail.com>
2021-11-11 16:30:19 -0500

    began server setup

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,13 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
 import {Post} from "./entities/post";
+import express from 'express';
 
 createConnection().then(async connection => {
 
-    console.log("Inserting a new user into the database...");
-    const post = new Post();
-    post.title = 'Test post';
-
-    await connection.manager.save(post);
-    console.log("Saved a new post with id: " + post.id);
-
-    console.log("Loading posts from the database...");
-    const posts = await connection.manager.find(Post);
-    console.log("Loaded posts: ", post);
-
-    console.log("Here you can setup and run express/koa/any other framework.");
-
+    const app = express();
+    app.get('/', (req, res) => {
+        console.log('Up and running');
+    });
+    app.listen(3000, () => console.log('Listening at 3000'));
 }).catch(error => console.log(error));
