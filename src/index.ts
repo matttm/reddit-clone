13e1e1b77cd449f3967efd09d2a4c4e8e44f83cb
matttm <matttmaloney@gmail.com>
2021-11-13 09:18:02 -0500

    building resolver

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
-import {Post} from "./entities/post";
 import express from 'express';
+import {ApolloServer} from "apollo-server-express";
+import {buildSchema} from "type-graphql";
+import {HelloResolver} from "./resolvers/hello.resolver";
 
 createConnection().then(async connection => {
 
+    const server = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    })
     const app = express();
-    app.get('/', (req, res) => {
-        console.log('Up and running');
-    });
+    server.applyMiddleware({ app });
+
     app.listen(3000, () => console.log('Listening at 3000'));
 }).catch(error => console.log(error));
