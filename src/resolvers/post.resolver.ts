e4113555e4bba351b0891bc42d42bd70bada2734
matttm <matttmaloney@gmail.com>
2021-11-16 06:50:44 -0500

    fixing runtime issues

--- src/resolvers/post.resolver.ts
+++ src/resolvers/post.resolver.ts
@@ -1,11 +1,11 @@
-import {Ctx, Query, Resolver} from "type-graphql";
+import { Ctx, Query, Resolver } from 'type-graphql';
 import { Post } from '../entities/post';
-import {Context} from "../types";
+import { Context } from '../types';
 
 @Resolver()
 export class PostResolver {
     @Query(() => [Post])
-    posts(@Ctx() { postRepository }: Context) {
+    posts(@Ctx() { postRepository }: Context): Promise<Post[]> {
         return postRepository.find();
     }
 }
