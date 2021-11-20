3aa2ae1f4d4650826972ab747803701068704776
matttm <matttmaloney@gmail.com>
2021-11-20 12:32:01 -0500

    adding user migration

---
+++ src/migrations/1637429266822-user.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class user1637429266822 implements MigrationInterface {
+    name = 'user1637429266822';
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `CREATE TABLE \`user\` (\`id\` int NOT NULL AUTO_INCREMENT, \`username\` varchar(255) NOT NULL, \`password\` varchar(255) NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), UNIQUE INDEX \`IDX_78a916df40e02a9deb1c4b75ed\` (\`username\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `DROP INDEX \`IDX_78a916df40e02a9deb1c4b75ed\` ON \`user\``
+        );
+        await queryRunner.query(`DROP TABLE \`user\``);
+    }
+}
