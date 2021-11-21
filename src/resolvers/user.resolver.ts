f10809cd3df9724683031492fbdc5d7f4426f70a
matttm <matttmaloney@gmail.com>
2021-11-21 10:47:47 -0500

    added login to resolver

--- src/resolvers/user.resolver.ts
+++ src/resolvers/user.resolver.ts
@@ -1,5 +1,13 @@
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from 'type-graphql';
-import { hash } from 'argon2';
+import {
+    Arg,
+    Ctx,
+    Field,
+    InputType,
+    Mutation,
+    Query,
+    Resolver
+} from 'type-graphql';
+import { hash, verify } from 'argon2';
 
 import { Context } from '../types';
 import { User } from '../entities/user';
@@ -14,7 +22,7 @@ export class Credentials {
 
 @Resolver()
 export class UserResolver {
-    @Mutation(() => User)
+    @Mutation(() => User, { nullable: true })
     async register(
         @Arg('credentials') credentials: Credentials,
         @Ctx() { userRepository }: Context
@@ -24,7 +32,7 @@ export class UserResolver {
             username
         });
         if (taken) {
-            return Promise.resolve(null);
+            return null;
         }
         return userRepository.save(
             userRepository.create({
@@ -32,5 +40,36 @@ export class UserResolver {
                 password: await hash(password)
             })
         );
+    }
+
+    @Mutation(() => User, { nullable: true })
+    async login(
+        @Arg('credentials') credentials: Credentials,
+        @Ctx() { userRepository }: Context
+    ): Promise<User | null> {
+        const { username, password } = credentials;
+        const user = await userRepository.findOne({
+            username
+        });
+        if (!user) {
+            return null;
+        }
+        console.log(`User ${username} exists`);
+        const matches = await verify(user.password, password);
+        if (!matches) {
+            console.log(
+                `User ${username} provided an incorrect password: ${matches}`
+            );
+            return null;
+        }
+        console.log(
+            `User ${username} provided the correct password: ${matches}`
+        );
+        return user;
+    }
+
+    @Query(() => [User])
+    users(@Ctx() { userRepository }: Context): Promise<User[]> {
+        return userRepository.find();
     }
 }
