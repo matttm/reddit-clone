2482c0b47d14a331a57264b160e71c0fa361401c
matttm <matttmaloney@gmail.com>
2021-12-19 20:28:00 -0500

    fixing init

--- backend/initialize.sh
+++ backend/initialize.sh
@@ -2,8 +2,12 @@
 
 # builds it
 npm run dev &
+server=$!
 
 sleep 10
 
 # run migrations to create db structure
 npx typeorm migration:run
+
+read  -n 1 -p "Press any key to kill server:" mainmenuinput
+kill -9 $server
