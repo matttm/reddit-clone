3471c5000975d8839069df3a925ac92b5872164a
matttm <matttmaloney@gmail.com>
2022-01-18 07:46:49 -0500

    debugging hanging port on init

--- backend/initialize.sh
+++ backend/initialize.sh
@@ -1,13 +1,13 @@
 !bin/bash
 
 # builds it
-npm run dev &
-server=$!
+# npm run dev &
+# server=$!
 
 sleep 10
 
 # run migrations to create db structure
 npx typeorm migration:run
 
-read  -n 1 -p "Press any key to kill server:" mainmenuinput
-kill -9 $server
+# read  -n 1 -p "Press any key to kill server:" mainmenuinput
+# kill -9 $server
