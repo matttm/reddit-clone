caba84fe5c17424f36eb9519f9bd88b6a93e7d09
matttm <matttmaloney@gmail.com>
2022-08-26 14:59:30 -0400

    making reactive navbar

--- frontend/src/components/NavItem.tsx
+++ frontend/src/components/NavItem.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Box, Link } from '@chakra-ui/core';
+import { useRouter } from 'next/router';
 
 const NavItem: React.FC<any> = props => {
-    const { active, navTo, onClick } = props;
+    const router = useRouter();
+    const { navTo, onClick } = props;
+    const active = router.pathname === navTo;
     return (
         <Box border={active ? '1px' : 0} borderBottomColor={'white'}>
             <Link href={navTo} color={'white'} onClick={onClick}>
