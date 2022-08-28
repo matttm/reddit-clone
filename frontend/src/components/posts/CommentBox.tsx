a7dc4e89c3233b5063443860c3b85c27d067b2b9
matttm <matttmaloney@gmail.com>
2022-08-28 18:04:09 -0400

    fixed comment box

--- frontend/src/components/posts/CommentBox.tsx
+++ frontend/src/components/posts/CommentBox.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import { InputField } from '../utilities/InpurField';
 import { InputTypeEnum } from '../../types';
 import { Button, Flex } from '@chakra-ui/core';
+import { Formik } from 'formik';
 
 const CommentBox: React.FC<any> = () => {
     return (
         <Flex direction={'column'} padding={'10px'}>
-            <InputField
-                name={'comment-box'}
-                label={'Post a comment'}
-                placeholder="body"
-                inputTypeString={InputTypeEnum.TEXTAREA}></InputField>
-            <Button marginTop={8} type="submit" variantColor="green">
-                Create
-            </Button>
+            <Formik initialValues={{ comment: '' }} onSubmit={() => {}}>
+                <form>
+                    <InputField
+                        name={'comment'}
+                        label={'Post a comment'}
+                        placeholder="..."
+                        inputTypeString={InputTypeEnum.TEXTAREA}></InputField>
+                    <Button marginTop={8} type="submit" variantColor="green">
+                        Comment
+                    </Button>
+                </form>
+            </Formik>
         </Flex>
     );
 };
