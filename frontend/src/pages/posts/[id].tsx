03d80a5733815c44bf58d6f3fedc23d967dcf341
matttm <matttmaloney@gmail.com>
2022-08-28 15:43:46 -0400

    adding comment box

--- frontend/src/pages/posts/[id].tsx
+++ frontend/src/pages/posts/[id].tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { usePostQuery } from '../../generated/graphql';
 import { Flex, FormHelperText, Text } from '@chakra-ui/core';
 import { useRouter } from 'next/router';
+import { CommentNode } from 'node-html-parser';
+import CommentBox from '../../components/posts/CommentBox';
 
 export const Id: React.FC<any> = () => {
     const router = useRouter();
@@ -11,14 +13,19 @@ export const Id: React.FC<any> = () => {
     }
     const [result, reexecuteQuery] = usePostQuery({ variables: { id: +id } });
     const { data, fetching, error } = result;
-
-    console.log(result);
     if (fetching) return <p>Loading...</p>;
     if (error) return <p>Oh no... {error.message}</p>;
     if (!data) return <p>Data is corrupt</p>;
     return (
         <Flex direction={'column'}>
-            <Text>{data.post.title}</Text>
+            <Text fontSize={'18px'} fontWeight={'bold'}>
+                {data.post.title}
+            </Text>
+            <Text fontSize={'14px'} fontWeight={'light'}>
+                Created at {data.post.createdAt}
+            </Text>
+            <Text fontSize={'16px'}>{data.post.body}</Text>
+            <CommentBox></CommentBox>
         </Flex>
     );
 };
