73750e75396fd7e60cf7c52f6c04f31be3d94b1d
matttm <matttmaloney@gmail.com>
2022-08-28 11:25:48 -0400

    post styling

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Flex, Text } from '@chakra-ui/core';
+import theme from '../theme';
 
 const Post: React.FC<any> = ({ title, createdAt, person }) => {
     return (
-        <Flex padding={'10px'} direction={'column'}>
+        <Flex bg={'#252531'} padding={'10px'} direction={'column'}>
             <Text fontSize={'16px'} fontWeight={'bold'}>
                 {title}
             </Text>
-            <Text fontSize={'12px'} fontWeight={'light'} color={'gray'}>
+            <Text fontSize={'12px'} fontWeight={'light'} color={'lightgrey'}>
                 Created by {person.username} at {createdAt}
             </Text>
         </Flex>
