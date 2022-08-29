ef598e6a760eb80db0d0dc982f1693f0fc0ed051
matttm <matttmaloney@gmail.com>
2022-08-29 07:02:43 -0400

    adding login condition for comment box

--- frontend/src/components/posts/CommentBox.tsx
+++ frontend/src/components/posts/CommentBox.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import { InputField } from '../utilities/InpurField';
 import { InputTypeEnum } from '../../types';
-import { Button, Flex } from '@chakra-ui/core';
+import { Button, Flex, Text } from '@chakra-ui/core';
 import { Formik } from 'formik';
+import { isAuthenticated } from '../../services/authentication.service';
 
 const CommentBox: React.FC<any> = () => {
+    if (!isAuthenticated()) {
+        return (
+            <Flex
+                direction={'column'}
+                padding={'30px'}
+                border={'3px'}
+                borderColor={'black'}
+                alignItems={'center'}
+                justifyItems={'center'}>
+                <Text>You must have an account to comment.</Text>
+                <Text>
+                    <a href={'/login'}>Login</a> or{' '}
+                    <a href={'/register'}>Register</a>
+                </Text>
+            </Flex>
+        );
+    }
     return (
         <Flex direction={'column'} padding={'10px'}>
             <Formik initialValues={{ comment: '' }} onSubmit={() => {}}>
