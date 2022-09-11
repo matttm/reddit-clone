e6a8e890bc54cfe4ec15bdf118ff48e5e2eda171
matttm <matttmaloney@gmail.com>
2022-09-11 09:13:28 -0400

    working on auth ctx

--- frontend/src/context/GlobalContextProvider.tsx
+++ frontend/src/context/GlobalContextProvider.tsx
@@ -1,14 +1,33 @@
-import React, {useState,} from "react";
+import React, {useContext, useEffect, useState,} from "react";
 import { GlobalContext } from "./GlobalContext";
-import {useIsAuthenticatedQuery} from "../generated/graphql";
+import {Person, useIsAuthenticatedQuery} from "../generated/graphql";
 
-export const GlobalContextProvide: React.FC<any> = ({ isAuthenticated, personinfo, children }) => {
+export const GlobalContextProvide: React.FC<any> = ({ children }) => {
     const [isLoading, setIsLoading] = useState(true);
+    const [person, setPerson] = useState(null as unknown as Person);
+    const [isAuthenticated, setAuthenticated] = useState(true);
 
+    const getAuthentication = () => {
+        const [result, reexecuteQuery] = useIsAuthenticatedQuery();
+        const data = result.data?.isAuthenticated;
+        const isAuthenticated = data?.validationErrors?.errors?.length === 0;
+        const personInfo = data?.person;
+        return ({
+            isAuthenticated,
+            personInfo
+        })
+    };
+    useEffect(() => {
+        setIsLoading(true);
+        const payload = getAuthentication();
+        setPerson(payload.personInfo as Person);
+        setAuthenticated(payload.isAuthenticated);
+        setIsLoading(false);
+    }, []);
     return (
         <GlobalContext.Provider
             value={{
-                person: personinfo,
+                person,
                 isAuthenticated: isAuthenticated,
                 loading: isLoading,
                 setLoading: setIsLoading,
