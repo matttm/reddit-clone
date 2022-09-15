97b082c77f6adfae534d159d418eeb6acbd288e1
matttm <matttmaloney@gmail.com>
2022-09-15 12:08:44 -0400

    fixed rendering error

--- frontend/src/context/GlobalContextProvider.tsx
+++ frontend/src/context/GlobalContextProvider.tsx
@@ -12,8 +12,13 @@ export const GlobalContextProvider: React.FC<any> = ({
     const [isLoading, setIsLoading] = useState(true);
     const [_isAuthenticated, setIsAuthenticated] = useState(false);
     const [person, setPerson] = useState(null as unknown as Person);
-    setIsAuthenticated(isAuthenticated);
-    setPerson(person);
+    useEffect(() => {
+        setIsLoading(true);
+        setIsAuthenticated(isAuthenticated);
+        setPerson(person);
+        setIsLoading(false);
+    }, []);
+    // const person = {}
     return (
         <GlobalContext.Provider
             value={{
@@ -28,14 +33,14 @@ export const GlobalContextProvider: React.FC<any> = ({
     );
 };
 
-export async function getIsAuthenticated() {
-    const [result, reexecuteQuery] = useIsAuthenticatedQuery();
-    const data = result.data?.isAuthenticated;
-    const isAuthenticated = data?.validationErrors?.errors?.length === 0;
-    const personInfo = data?.person;
-    console.log(`User is ${isAuthenticated ? '' : 'not '}authenticated`);
-    return {
-        isAuthenticated,
-        personInfo
-    };
-}
+// export async function getIsAuthenticated() {
+//     const [result, reexecuteQuery] = useIsAuthenticatedQuery();
+//     const data = result.data?.isAuthenticated;
+//     const isAuthenticated = data?.validationErrors?.errors?.length === 0;
+//     const personInfo = data?.person;
+//     console.log(`User is ${isAuthenticated ? '' : 'not '}authenticated`);
+//     return {
+//         isAuthenticated,
+//         personInfo
+//     };
+// }
