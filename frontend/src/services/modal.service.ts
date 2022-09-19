ad65421110a26a6431ea31a2762294c5d43b675f
matttm <matttmaloney@gmail.com>
2022-09-19 08:03:29 -0400

    importing my modal servive from the airbnb clone w brandon

---
+++ frontend/src/services/modal.service.ts
@@ -0,0 +1,28 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import '../../pages/GenericModal/GenericModal.css';
+import GenericModal from "../../pages/GenericModal/GenericModal";
+
+const createModalServiceSingleton = () => {
+    // container and modals are wrapped in the closure
+    const container = document.getElementById('portal-container');
+    let modalRef = null;
+    const openModal = (html) => {
+        modalRef = ReactDOM.render(
+            <GenericModal>
+                {html}
+            </GenericModal>,
+        container
+    );
+    };
+    const closeModal = () => {
+        modalRef = null;
+        ReactDOM.unmountComponentAtNode(container);
+    };
+    return Object.freeze({
+        openModal,
+        closeModal
+    });
+}
+
+export const modalService = createModalServiceSingleton();
