a373ccdc85be27ca7d3a13eff4d5938738e973cd
matttm <matttmaloney@gmail.com>
2022-09-20 10:09:32 -0400

    working on deletinmg button

--- frontend/src/components/posts/DeletePost.tsx
+++ frontend/src/components/posts/DeletePost.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 const DeletePost: React.FC<any> = ({ postId, children }) => {
+    const [, deletePost] = useDeletePost();
     return (
-        <p>
-            Are you sure you want to delete this post?
-        </p>
+        <>
+            <p>
+                Are you sure you want to delete this post?
+            </p>
+            <form>
+                <button type={'submit'}>Confirm</button>
+                <button>Cancel</button>
+            </form>
+        </>
     );
 }
 
