e6ec265778e24e7c71d409e0a0f637be2df29c32
matttm <matttmaloney@gmail.com>
2022-09-21 19:45:02 -0400

    rigging

--- frontend/src/components/posts/DeletePost.tsx
+++ frontend/src/components/posts/DeletePost.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, {useContext} from "react";
 import {useDeletePostMutation} from "../../generated/graphql";
 import {Button, Flex, Text} from "@chakra-ui/core";
 import Wrapper from "../utilities/Wrapper";
 import {VariantsEnum} from "../../types";
 import {Form, Formik} from "formik";
 import {useRouter} from "next/router";
+import {GlobalContext} from "../../context/GlobalContext";
 
 const DeletePost: React.FC<any> = ({ postId, children }) => {
+    const { modalService } = useContext(GlobalContext);
     const router = useRouter();
     const [, deletePost] = useDeletePostMutation();
     return (
@@ -15,19 +17,29 @@ const DeletePost: React.FC<any> = ({ postId, children }) => {
                 <Formik
                     initialValues={{ postId }}
                     onSubmit={async (values) => {
-                        // router.push('/posts');
+                        const res = await deletePost(postId);
+                        router.push('/posts');
                         return values;
                     }}>
                     {({ isSubmitting }) => (
-                        <Flex direction={'row'}>
+                        <Flex direction={'column'}>
                             <Text>Are you sure you want to delete this post?</Text>
-                            <Button
-                                marginTop={8}
-                                type="submit"
-                                isLoading={isSubmitting}
-                                variantColor="green">
-                                Confirm
-                            </Button>
+                            <Flex direction={'row'}>
+                                <Button
+                                    marginTop={8}
+                                    type="submit"
+                                    isLoading={isSubmitting}
+                                    variantColor="green">
+                                    Confirm
+                                </Button>
+                                <Button
+                                    marginTop={8}
+                                    isLoading={isSubmitting}
+                                    onClick={() => modalService?.closeModal()}
+                                    variantColor="gray">
+                                    Close
+                                </Button>
+                            </Flex>
                         </Flex>
                     )}
                 </Formik>
