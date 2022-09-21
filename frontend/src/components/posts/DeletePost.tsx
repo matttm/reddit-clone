f5c0817d6502a3d5c704ac836d5f975fdb6c1aa7
matttm <matttmaloney@gmail.com>
2022-09-21 16:38:23 -0400

    wip for prettying up delete

--- frontend/src/components/posts/DeletePost.tsx
+++ frontend/src/components/posts/DeletePost.tsx
@@ -1,21 +1,35 @@
 import React from "react";
 import {useDeletePostMutation} from "../../generated/graphql";
-import {Flex, Text} from "@chakra-ui/core";
+import {Button, Flex, Text} from "@chakra-ui/core";
+import Wrapper from "../utilities/Wrapper";
+import {VariantsEnum} from "../../types";
+import {Form, Formik} from "formik";
+import {useRouter} from "next/router";
 
 const DeletePost: React.FC<any> = ({ postId, children }) => {
+    const router = useRouter();
     const [, deletePost] = useDeletePostMutation();
     return (
-        <>
-            <Flex direction={'column'} color={'black'}>
-                <Text>
-                    Are you sure you want to delete this post?
-                </Text>
-                <form>
-                    <button type={'submit'}>Confirm</button>
-                    <button>Cancel</button>
-                </form>
-            </Flex>
-        </>
+        <Wrapper variant={VariantsEnum.regular.description}>
+            <Formik
+                initialValues={{ postId: 1 }}
+                onSubmit={async (values) => {
+                    router.push('/posts');
+                    return null;
+                }}>
+                {({ isSubmitting }) => (
+                    <Form>
+                        <Button
+                            marginTop={8}
+                            type="submit"
+                            isLoading={isSubmitting}
+                            variantColor="green">
+                            Login
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
+        </Wrapper>
     );
 }
 
