1303f98e2447ad47140dc7331962f77eafe2edb0
matttm <matttmaloney@gmail.com>
2022-09-27 10:20:41 -0400

    using new render api

--- frontend/src/services/modal.service.tsx
+++ frontend/src/services/modal.service.tsx
@@ -11,13 +11,13 @@ export interface ModalService {
 export const createModalServiceSingleton: () => Readonly<ModalService> = () => {
     // container and modals are wrapped in the closure
     const container = document.getElementById('portal-container');
+    if (!container) {
+        console.error('Error creating modal as container is null');
+        return {} as Readonly<ModalService>;
+    }
     const node = ReactDOMClient.createRoot(container);
     let modalRef = null;
     const openModal = (html: React.ReactElement) => {
-        if (!container) {
-            console.error('Error creating modal as container is null');
-            return;
-        }
         node.render(
             <GenericModal>
                 {html}
@@ -25,13 +25,8 @@ export const createModalServiceSingleton: () => Readonly<ModalService> = () => {
         );
     };
     const closeModal = () => {
-        modalRef = null;
-        console.log('container', container)
-        // only try to unmount if container exisrs
-        if (container) {
-            ReactDOM.unmountComponentAtNode(container);
-            console.log('Unmounting component');
-        }
+        node.unmount();
+        console.log('Unmounting component');
 
     };
     return Object.freeze({
