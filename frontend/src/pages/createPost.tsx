5d897074645ca9e022ca1ce23543dfa31be2da41
matttm <matttmaloney@gmail.com>
2022-09-30 15:25:09 -0400

    setting the update

--- frontend/src/pages/createPost.tsx
+++ frontend/src/pages/createPost.tsx
@@ -4,17 +4,32 @@ import Wrapper from '../components/utilities/Wrapper';
 import { InputTypeEnum, VariantsEnum } from '../types';
 import { InputField } from '../components/utilities/InpurField';
 import { Button } from '@chakra-ui/core';
-import { useCreatePostMutation } from '../generated/graphql';
+import {useCreatePostMutation, usePostQuery} from '../generated/graphql';
 import * as Yup from 'yup';
 import { useRouter } from 'next/router';
+import {bind} from "@wry/context";
 
 export const CreatePost: React.FC<any> = ({}) => {
     const router = useRouter();
     const [, post] = useCreatePostMutation();
+    let initialTitle = '';
+    let initialBody = '';
+    // if an id is present, we're going to edit
+    if (router.query?.id) {
+        const [result, reexecuteQuery] = usePostQuery({ variables: { id: +router.query.id } });
+        const data = result.data?.post;
+        const title = data?.title;
+        const body = data?.body;
+        // assert valid data, not an error
+        if (title && body) {
+            initialTitle = data?.title;
+            initialBody = data?.body;
+        }
+    }
     return (
         <Wrapper variant={VariantsEnum.regular.description}>
             <Formik
-                initialValues={{ title: '', body: '' }}
+                initialValues={{ title: initialTitle, body: initialBody }}
                 validationSchema={Yup.object().shape({})}
                 onSubmit={async (values, { setErrors }) => {
                     const res = await post(values);
