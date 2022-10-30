d99cca2c188bb871d796a5ed79ab9f6b82346c43
matttm <matttmaloney@gmail.com>
2022-10-30 15:15:10 -0400

    init jest

---
+++ frontend/tests/index.test.js
@@ -0,0 +1,11 @@
+import Home from "../src/pages/index";
+import "@testing-library/jest-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
+
+describe("Home", () => {
+    it("render Home text", () => {
+        render(<Home />);
+        // check if all components are rendered
+        expect(screen.getByText("Home")).toBeInTheDocument();
+    });
+});
