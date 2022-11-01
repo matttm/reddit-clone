2f26edff85cb8c86d07e701c64db4a740be4c6d3
matttm <matttmaloney@gmail.com>
2022-11-01 08:14:04 -0400

    working on action test

--- frontend/tests/actions/action.test.tsx
+++ frontend/tests/actions/action.test.tsx
@@ -1,6 +1,7 @@
 import {render, screen} from "@testing-library/react";
 import Action from "../../src/components/actions/Action";
 import "@testing-library/jest-dom";
+import {Simulate} from "react-dom/test-utils";
 
 describe("Action", () => {
     it("render print the children", () => {
@@ -10,6 +11,17 @@ describe("Action", () => {
             </Action>
         );
         // check if all components are rendered
-        expect(screen.getByText("Children")).toBeInTheDocument();
+        expect(screen.queryByText("Children")).toBeInTheDocument();
+    });
+    it("execute csllback on click", () => {
+        const fn = jest.fn();
+        const wrapper = render(
+            <Action action={fn}>
+                Children
+            </Action>
+        );
+        Simulate.click(wrapper.container.getElementsByTagName(Action.name)[0]);
+        // check if all components are rendered
+        expect(fn).toHaveBeenCalled();
     });
 });
