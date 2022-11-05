58bfa90a8ceeb9b4a9b5da893c86a6c812ac16c6
matttm <matttmaloney@gmail.com>
2022-11-05 15:40:45 -0400

    fixing action bar test

--- frontend/tests/actions/ActionBar.test.tsx
+++ frontend/tests/actions/ActionBar.test.tsx
@@ -8,22 +8,86 @@ import {MockGlobalContext} from "../mocks/GlobalContext.mock";
 import ActionBar from "../../src/components/actions/ActionBar";
 import {ModalContextProvider} from "../../src/context/ModalContextProvider";
 
+const routerMock = {
+    route: '/',
+    pathname: '',
+    query: '',
+    asPath: '',
+    push: jest.fn(),
+    events: {
+        on: jest.fn(),
+        off: jest.fn()
+    },
+    beforePopState: jest.fn(() => null),
+    prefetch: jest.fn(() => null)
+};
+
+jest.mock('next/router', () => ({
+    useRouter() {
+        return ({ ...routerMock });
+    },
+}));
+
 describe("ActionBar", () => {
-    it("render display two locks if unauthorized", () => {
+    describe('when unauthorized', () => {
+        it("render display two locks", () => {
+            const setModal = jest.fn();
+            const dom = render(
+                <GlobalContext.Provider value={{...MockGlobalContext, isAuthenticated: false}}>
+                    <ModalContextProvider value={{setModal}}>
+                        <ActionBar/>
+                    </ModalContextProvider>
+                </GlobalContext.Provider>
+            );
+            const trash = dom.container.querySelector('#delete-action');
+            const edit = dom.container.querySelector('#edit-action');
+            expect(trash).toBeTruthy();
+            expect(edit).toBeTruthy();
+            expect(trash).toBeInTheDocument();
+            expect(edit).toBeInTheDocument();
+            expect(screen.getAllByTitle("lock-icon").length).toBe(2);
+        });
+    });
+    describe('when authorized', () => {
         const setModal = jest.fn();
-        const dom = render(
-            <GlobalContext.Provider value={{ ...MockGlobalContext, isAuthenticated: false }}>
+        const html = (
+            <GlobalContext.Provider value={{ ...MockGlobalContext, isAuthenticated: true }}>
                 <ModalContextProvider value={{ setModal }}>
                     <ActionBar />
                 </ModalContextProvider>
             </GlobalContext.Provider>
         );
-        const trash = dom.container.querySelector('#delete-action');
-        const edit = dom.container.querySelector('#edit-action');
-        expect(trash).toBeTruthy();
-        expect(edit).toBeTruthy();
-        expect(trash).toBeInTheDocument();
-        expect(edit).toBeInTheDocument();
-        expect(screen.getAllByTitle("lock-icon").length).toBe(2);
-    });
+        it("render display icons", () => {
+            const dom = render(html);
+            let trash = dom.container.querySelector('#delete-action');
+            let edit = dom.container.querySelector('#edit-action');
+            expect(trash).toBeTruthy();
+            expect(edit).toBeTruthy();
+            expect(trash).toBeInTheDocument();
+            expect(edit).toBeInTheDocument();
+            trash = dom.container.querySelector('#delete-icon');
+            edit = dom.container.querySelector('#edit-icon');
+            expect(trash).toBeTruthy();
+            expect(edit).toBeTruthy();
+            expect(trash).toBeInTheDocument();
+            expect(edit).toBeInTheDocument();
+            expect(() => screen.getAllByTitle("lock-icon")).toThrow();
+        });
+        it("call setModal on delete click", () => {
+            const dom = render(html);
+            const trash = dom.container.querySelector('#delete-icon');
+            expect(trash).toBeTruthy();
+            expect(trash).toBeInTheDocument();
+            fireEvent.click(trash as Element);
+            expect(setModal).toHaveBeenCalled();
+        });
+        it("call route on delete click", () => {
+            const dom = render(html);
+            const edit = dom.container.querySelector('#edit-icon');
+            expect(edit).toBeTruthy();
+            expect(edit).toBeInTheDocument();
+            fireEvent.click(edit as Element);
+            expect(routerMock.push).toHaveBeenCalled();
+        });
+    })
 });
