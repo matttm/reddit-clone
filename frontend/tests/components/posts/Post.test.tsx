d21b4954ef8d54b63bb83b100fe16f1a6b486f0b
matttm <matttmaloney@gmail.com>
2022-11-19 09:15:16 -0500

    writing post test

--- frontend/tests/components/posts/Post.test.tsx
+++ frontend/tests/components/posts/Post.test.tsx
@@ -1,9 +1,9 @@
 import "@testing-library/jest-dom";
-import {render} from "@testing-library/react";
+import {fireEvent, render} from "@testing-library/react";
 import {RouterMock} from "../../mocks/Router.mock";
 import {beforeEach} from "@jest/globals";
 import React from "react";
-import {getByText} from "@testing-library/dom";
+import {getByText, waitFor} from "@testing-library/dom";
 import {Navbar} from "../../../src/components/navigation/Navbar";
 import Post from "../../../src/components/posts/Post";
 import {Person} from "../../../src/generated/graphql";
@@ -15,19 +15,27 @@ jest.mock('next/router', () => ({
     },
 }));
 describe("Post", () => {
+    const id = 1;
+    const title = 'Help with the giant dragon';
+    const date = '11-09-22';
+    const person: Person = {createdAt: undefined, id: "", updatedAt: undefined, username: 'matttm' };
     beforeEach(() => {
         jest.restoreAllMocks();
     });
     it('should display id, title, date, author', () => {
-        const id = 1;
-        const title = 'Help with the giant dragon';
-        const date = '11-09-22';
-        const person: Person = {createdAt: undefined, id: "", updatedAt: undefined, username: 'matttm' };
         const dom = render(
             <Post id={id} title={title} createdAt={date} person={person} />
         );
         const container = dom.container;
         expect(() => getByText(container, `Created by ${person.username} at ${date}`)).not.toThrow();
         expect(() => getByText(container, title)).not.toThrow();
+    });
+    it('should display id, title, date, author', async () => {
+        const dom = render(
+            <Post id={id} title={title} createdAt={date} person={person} />
+        );
+        const container = dom.container;
+        const anchor = container.querySelector('a');
+        expect(anchor).toHaveAttribute('href', `/posts/${id}`);
     });
 });
