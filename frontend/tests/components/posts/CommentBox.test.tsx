ab91eeaf9f7cc900668b899364ff6a81becdaa63
matttm <matttmaloney@gmail.com>
2022-11-20 09:02:42 -0500

    working on comment box test

--- frontend/tests/components/posts/CommentBox.test.tsx
+++ frontend/tests/components/posts/CommentBox.test.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import {GlobalContext} from "../../../src/context/GlobalContext";
 import {MockGlobalContext} from "../../mocks/GlobalContext.mock";
 import CommentBox from "../../../src/components/posts/CommentBox";
+import {render} from "@testing-library/react";
+import {ThemeProvider} from "@chakra-ui/core";
 
 describe("CommentBox", () => {
     beforeEach(() => {
@@ -11,16 +13,34 @@ describe("CommentBox", () => {
     });
     describe('when logged in', () => {
         const html = (
-            <GlobalContext.Provider value={{...MockGlobalContext, isAuthenticated: true }}>
-                <CommentBox />
-            </GlobalContext.Provider>
+            <ThemeProvider>
+                <GlobalContext.Provider value={{...MockGlobalContext, isAuthenticated: true }}>
+                    <CommentBox />
+                </GlobalContext.Provider>
+            </ThemeProvider>
         );
+        it('should have an input field', () => {
+            const dom = render(html);
+            dom.debug()
+            expect(dom.container.querySelector('textarea')).toBeInTheDocument();
+            expect(() => dom.getByText('Comment')).not.toThrow();
+            expect(() => dom.getByText('You must have an account to comment.')).toThrow();
+        })
     });
     describe('when not logged in', () => {
         const html = (
-            <GlobalContext.Provider value={{...MockGlobalContext, isAuthenticated: false }}>
-                <CommentBox />
-            </GlobalContext.Provider>
+            <ThemeProvider>
+                <GlobalContext.Provider value={{...MockGlobalContext, isAuthenticated: false }}>
+                    <CommentBox />
+                </GlobalContext.Provider>
+            </ThemeProvider>
         );
+        it('should not have an input field', () => {
+            const dom = render(html);
+            dom.debug()
+            expect(dom.container.querySelector('textarea')).not.toBeInTheDocument();
+            expect(() => dom.getByText('Comment')).toThrow();
+            expect(() => dom.getByText('You must have an account to comment.')).not.toThrow();
+        })
     });
 });
