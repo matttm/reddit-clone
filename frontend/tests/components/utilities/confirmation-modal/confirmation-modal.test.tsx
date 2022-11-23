bc744418585237b1a330dd0ee3d484bbfff8c6c7
matttm <matttmaloney@gmail.com>
2022-11-23 18:06:20 -0500

    working on HOC test

--- frontend/tests/components/utilities/confirmation-modal/confirmation-modal.test.tsx
+++ frontend/tests/components/utilities/confirmation-modal/confirmation-modal.test.tsx
@@ -1,13 +1,15 @@
 
 import "@testing-library/jest-dom";
-import {getByText} from "@testing-library/dom";
-import {render} from "@testing-library/react";
+import {getByText, waitFor} from "@testing-library/dom";
+import {fireEvent, render, RenderResult} from "@testing-library/react";
 import {GlobalContext} from "../../../../src/context/GlobalContext";
 import {ModalContext} from "../../../../src/context/ModalContext";
 import {Text, ThemeProvider} from "@chakra-ui/core";
 import {withConfirmationModal} from "../../../../src/components/utilities/confirmation-modal/confirmation-modal";
 import {MockGlobalContext} from "../../../mocks/GlobalContext.mock";
 import {MockModalContext} from "../../../mocks/ModalContext.mock";
+import {beforeEach} from "@jest/globals";
+import {awaitExpression} from "@babel/types";
 
 describe("ConfirmationModal", () => {
     const dummyContent = () => <Text>Dummy</Text>;
@@ -17,16 +19,31 @@ describe("ConfirmationModal", () => {
         mutationFake
     ]) as any;
     const DummyComponent = withConfirmationModal(dummyContent, useMutationFake);
-    it('should', () => {
-        const html = (
-            <ThemeProvider>
-                <GlobalContext.Provider value={{...MockGlobalContext}} >
-                    <ModalContext.Provider value={{...MockModalContext}} >
-                        <DummyComponent></DummyComponent>
-                    </ModalContext.Provider>
-                </GlobalContext.Provider>
-            </ThemeProvider>
-        )
-        expect(() => render(html).getByText('Dummy')).not.toThrow();
+    const html = (
+        <ThemeProvider>
+            <GlobalContext.Provider value={{...MockGlobalContext}}>
+                <ModalContext.Provider value={{...MockModalContext}}>
+                    <DummyComponent></DummyComponent>
+                </ModalContext.Provider>
+            </GlobalContext.Provider>
+        </ThemeProvider>
+    );
+    let dom: RenderResult<typeof import("@testing-library/dom/types/queries"), HTMLElement, HTMLElement>;
+    beforeEach(() => {
+        dom = render(html);
     })
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+    it('should render component',  () => {
+        expect(() => dom.getByText('Dummy')).not.toThrow();
+        expect(useMutationFake).toHaveBeenCalled();
+    });
+    it('should call mutation on click', async () => {
+        expect(() => dom.getByText('Dummy')).not.toThrow();
+        fireEvent.click(dom.getByText('Confirm'));
+        await waitFor(() => {
+            expect(mutationFake).toHaveBeenCalled();
+        });
+    });
 });
